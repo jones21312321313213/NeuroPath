4f733165779d509a4ddeec425aee9f32679685d9
jones21312321313213 <skinnyjones212121@gmail.com>
2026-05-23 15:03:50 +0800

    added things in the admin of each app

--- neuropath-backend/tracking/admin.py
+++ neuropath-backend/tracking/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ProgressReport, SecurityLog,StudentProcessingLog,AIGenerationLog
+
+admin.site.register(ProgressReport)
+admin.site.register(SecurityLog)
+admin.site.register(StudentProcessingLog)
+admin.site.register(AIGenerationLog)
